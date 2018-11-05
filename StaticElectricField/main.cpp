196316ce17c6df7d5aed808fcae5b298ba21dba9
Anton Vasin <mail@vasin.space>
2018-11-06 01:53:23 +0300

    Intial commit of the static electric field calculator

---
+++ StaticElectricField/main.cpp
@@ -0,0 +1,100 @@
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+//#include "cuda_runtime.h"
+#include "cuda_gl_interop.h"
+
+GLuint electricFieldTexture = -1;
+cudaGraphicsResource* cudaTextureResource = nullptr;
+
+void InitScene()
+{
+	GLenum error;
+	cudaError_t cudaError;
+
+	glEnable(GL_TEXTURE_2D);
+	error = glGetError();
+
+	glGenTextures(1, &electricFieldTexture);
+	error = glGetError();
+
+	glBindTexture(GL_TEXTURE_2D, electricFieldTexture);
+	error = glGetError();
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	error = glGetError();
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	error = glGetError();
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	error = glGetError();
+	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	error = glGetError();
+
+	float pixels[] =
+	{
+		1.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f
+	};
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_FLOAT, pixels);
+	error = glGetError();
+}
+
+void DisplayFunc()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	//glColor3f(0.0f, 0.0f, 0.0f);
+	glTexCoord2f(0.0, 0.0);
+	glVertex2f(-1.0f, -1.0f);
+
+	//glColor3f(1.0f, 0.0f, 0.0f);
+	glTexCoord2f(1.0, 0.0);
+	glVertex2f(1.0f, -1.0f);
+
+	//glColor3f(0.0f, 1.0f, 0.0f);
+	glTexCoord2f(1.0, 1.0);
+	glVertex2f(1.0f, 1.0f);
+
+	//glColor3f(0.0f, 0.0f, 1.0f);
+	glTexCoord2f(0.0, 1.0);
+	glVertex2f(-1.0f, 1.0f);
+	glEnd();
+
+	glutSwapBuffers();
+	glutPostRedisplay();
+}
+
+void MouseFunc(int button, int state, int x, int y)
+{
+}
+
+void ReshapeFunc(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(1366, 768);
+	glutCreateWindow("Static electric field");
+
+	glutReshapeFunc(ReshapeFunc);
+	glutDisplayFunc(DisplayFunc);
+	glutMouseFunc(MouseFunc);
+
+	auto err = glewInit();
+	InitScene();
+
+	glutMainLoop();
+
+	return 0;
+}

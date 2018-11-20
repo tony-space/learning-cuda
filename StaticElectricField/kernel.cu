115265b4c6b394d2147b10762fccdc218a6c3e62
Anton Vasin <mail@vasin.space>
2018-11-21 02:25:09 +0300

    Static electric field computation

--- StaticElectricField/kernel.cu
+++ StaticElectricField/kernel.cu
@@ -13,8 +13,8 @@
 #include <cassert>
 #include "kernel.hpp"
 
-__device__ static const float kElectronCharge = -1.60217622e-19f; //coulombs 
-__device__ static const float kElectricConstant = 8.854187817e-12f; //vacuum permittivity
+__device__ static const float2 kElectronPosition = { -0.3f, 0.0f };
+__device__ static const float2 kProtonPosition = { +0.3f, 0.0f };
 
 thrust::device_vector<float> electricField;
 
@@ -23,37 +23,67 @@ __global__ void electricFieldKernel(float* grid, unsigned width, unsigned height
 	auto x = blockIdx.x * blockDim.x + threadIdx.x;
 	auto y = blockIdx.y * blockDim.y + threadIdx.y;
 
-	if (x >= width || y >= height)
-		return;
-
-	//get position coords from -1 to +1
-	float2 position =
+	if (x < width && y < height)
 	{
-		(x / (float)(width - 1)) * 2.0f - 1.0f,
-		(y / (float)(height - 1)) * 2.0f - 1.0f
-	};
+		//get position coords from -1 to +1
+		float2 pixelPosition =
+		{
+			(x / (float)(width - 1)) * 2.0f - 1.0f,
+			(y / (float)(height - 1)) * 2.0f - 1.0f
+		};
 
-	float invDistance = 1.0f / (position.x * position.x + position.y * position.y);
-	if (isnan(invDistance) || isinf(invDistance))
-		return;
+		float dx;
+		float dy;
+		float distanceSq;
 
-	float field = kElectronCharge / (kElectricConstant * 4.0f * CUDART_PI_F) / invDistance;
+		dx = kElectronPosition.x - pixelPosition.x;
+		dy = kElectronPosition.y - pixelPosition.y;
+		distanceSq = dx * dx + dy * dy;
 
-	grid[x + y * width] = field;
+		float electronField = -0.01 / distanceSq;
+
+		if (distanceSq < 1e-4)
+		{
+			grid[x + y * width] = 0.0f;
+			return;
+		}
+
+
+		dx = kProtonPosition.x - pixelPosition.x;
+		dy = kProtonPosition.y - pixelPosition.y;
+		distanceSq = dx * dx + dy * dy;
+
+		if (distanceSq < 1e-4)
+		{
+			grid[x + y * width] = 0.0f;
+			return;
+		}
+
+		float protonField = 1 / distanceSq;
+
+
+		grid[x + y * width] = electronField + protonField;
+	}
 }
 
-__global__ void renderFieldKernel(float* grid, cudaSurfaceObject_t surfObj, unsigned width, unsigned height, float* min, float* max)
+__global__ void renderFieldKernel(float* grid, cudaSurfaceObject_t surfObj, unsigned width, unsigned height)
 {
 	auto x = blockIdx.x * blockDim.x + threadIdx.x;
 	auto y = blockIdx.y * blockDim.y + threadIdx.y;
 
-	float field = grid[x + y * width];
+	if (x < width && y < height)
+	{
+		float field = grid[x + y * width] / 3.0f;
 
-	field = (field - *min) / (*max - *min);
+		float4 result = { 0, 0, 0, 1.0f };
 
-	float4 result = { field, field, field, field };
+		if (field >= 0.0f)
+			result.x = field;
+		else
+			result.z = -field;
 
-	surf2Dwrite(result, surfObj, x * sizeof(result), y);
+		surf2Dwrite(result, surfObj, x * sizeof(result), y);
+	}
 }
 
 void ProcessElectronField(unsigned textureId, unsigned width, unsigned height)
@@ -84,21 +114,19 @@ void ProcessElectronField(unsigned textureId, unsigned width, unsigned height)
 
 	dim3 blockDim(32, 32); //32*32 = 1024 threads per block
 	dim3 gridDim((width - 1) / 32 + 1, (height - 1) / 32 + 1);
-	
+
 	electricFieldKernel <<<gridDim, blockDim>>> (electricField.data().get(), width, height);
 	error = cudaGetLastError();
 	assert(!error);
 
-	auto pair = thrust::minmax_element(thrust::device,electricField.begin(), electricField.end());
-	
-	float* d_min = thrust::raw_pointer_cast(&(*pair.first));
-	float* d_max = thrust::raw_pointer_cast(&(*pair.second));
+	/*auto pair = thrust::minmax_element(thrust::device, electricField.begin(), electricField.end());
 
-	renderFieldKernel <<<gridDim, blockDim >>> (electricField.data().get(), cuSurfaceObject, width, height, d_min, d_max);
+	float* d_min = thrust::raw_pointer_cast(&(*pair.first));
+	float* d_max = thrust::raw_pointer_cast(&(*pair.second));*/
+
+	renderFieldKernel <<<gridDim, blockDim>>> (electricField.data().get(), cuSurfaceObject, width, height);
 	error = cudaGetLastError();
 	assert(!error);
-
-	//thr
 
 	error = cudaDeviceSynchronize();
 	assert(!error);

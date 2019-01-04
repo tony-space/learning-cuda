edf8f4a7e35fdac3dc2ed1f901847e05af7f7799
Anton Vasin <mail@vasin.space>
2019-01-04 22:32:34 +0300

    Benchmarks (host serial, host parallel, device parallel)

---
+++ AdvancedReduction/main.cpp
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <vector>
+#include <chrono>
+#include <cstdint>
+
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+constexpr char* kCacheName = "randcache.bin";
+constexpr int64_t kNumbers = 9ll * 1024ll * 1024ll * 1024ll / 4ll;
+//constexpr int64_t kNumbers = 1024ll * 1024ll * 1024ll;
+
+std::vector<float> GenerateAndWrite()
+{
+	std::vector<float> result;
+	result.reserve(kNumbers);
+
+	srand(42);
+	for (int64_t i = 0; i < kNumbers; ++i)
+		result.push_back(float(rand() % 11 - 5));
+
+	auto file = fopen(kCacheName, "wb");
+	assert(file != nullptr);
+	auto written = fwrite(result.data(), sizeof(float), kNumbers, file);
+	assert(written == kNumbers);
+	fclose(file);
+
+	return result;
+}
+
+size_t NumbersInCache()
+{
+	auto file = fopen(kCacheName, "rb");
+	if (file == nullptr)
+		return 0;
+
+	fseek(file, 0, SEEK_END);
+	auto pos = _ftelli64(file);
+	fclose(file);
+
+	return pos / sizeof(float);
+}
+
+std::vector<float> ReadNumbers()
+{
+	std::vector<float> result(kNumbers);
+
+	auto file = fopen(kCacheName, "rb");
+	auto red = fread(result.data(), sizeof(float), kNumbers, file);
+	assert(red == kNumbers);
+	fclose(file);
+
+	return result;
+}
+
+std::vector<float> GetNumbers()
+{
+	if (NumbersInCache() < kNumbers)
+		return GenerateAndWrite();
+
+	return ReadNumbers();
+}
+
+void cudaReduce(const std::vector<float>& data);
+
+int main(int argc, char** argv)
+{
+	std::chrono::high_resolution_clock::time_point begin;
+	std::chrono::high_resolution_clock::time_point end;
+	float controlResult = 0;
+
+	std::vector<float> hostNumbers = GetNumbers();
+	
+	printf("Serial CPU started...\r\n");
+	begin = std::chrono::high_resolution_clock::now();
+	controlResult = 0;
+#pragma loop(no_parallel)
+	for (int64_t i = 0; i < kNumbers; ++i)
+		controlResult += hostNumbers[i];
+	end = std::chrono::high_resolution_clock::now();
+
+
+	printf("Result: %.3f\r\n", controlResult);
+	printf("Elapsed time on CPU (serial): %.3f ms\r\n", float(std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()) * 1e-6f);
+
+
+	printf("Parallel CPU started...\r\n");
+	begin = std::chrono::high_resolution_clock::now();
+	controlResult = 0;
+	auto data = hostNumbers.data();
+#pragma omp parallel for reduction(+:controlResult)
+	for (int64_t i = 0; i < kNumbers; ++i)
+		controlResult += data[i];
+	end = std::chrono::high_resolution_clock::now();
+
+
+	printf("Result: %.3f\r\n", controlResult);
+	printf("Elapsed time on CPU (parallel): %.3f ms\r\n", float(std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()) * 1e-6f);
+
+
+	cudaReduce(hostNumbers);
+
+	system("pause");
+
+	return 0;
+}
--- AdvancedReduction/reduce.cu
+++ AdvancedReduction/reduce.cu
@@ -1,15 +1,9 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-#include <numeric>
-#include <algorithm>
-#include <cstdlib>
-#include <climits>
+
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
-#include <chrono>
 
-constexpr size_t kNumbers = 704185371;
-//constexpr size_t kNumbers = 704181;
 constexpr size_t kMaxBlockSize = 256;
 
 constexpr int kDefaultValue = 0;
@@ -31,7 +25,7 @@ static inline __device__ T warpReduce(T val)
 	return val;
 }
 
-__global__ void reduceKernel(const int* __restrict__ values, size_t size, int* __restrict__ output)
+__global__ void reduceKernel(const float* __restrict__ values, size_t size, float* __restrict__ output)
 {
 	auto val = kDefaultValue;
 
@@ -40,7 +34,7 @@ __global__ void reduceKernel(const int* __restrict__ values, size_t size, int* _
 	auto laneId = threadIdx.x % warpSize;
 	auto gridSize = blockDim.x * gridDim.x;
 
-	extern __shared__ int cache[];
+	extern __shared__ float cache[];
 	auto cacheSize = divCeil(blockDim.x, unsigned(warpSize)); //equals to amount of warps in blocks
 
 	if (threadId < size)
@@ -64,23 +58,12 @@ __global__ void reduceKernel(const int* __restrict__ values, size_t size, int* _
 		output[blockIdx.x] = val;
 }
 
-int main(int argc, char** argv)
+void cudaReduce(const std::vector<float>& data)
 {
-	srand(42);
-	thrust::host_vector<int> hostNumbers;
-	hostNumbers.reserve(kNumbers);
-	for (size_t i = 0; i < kNumbers; ++i)
-		hostNumbers.push_back(rand() % 101 - 50);
-
-	auto begin = std::chrono::high_resolution_clock::now();
-	auto controlResult = std::accumulate(hostNumbers.begin(), hostNumbers.end(), 0);
-	auto end = std::chrono::high_resolution_clock::now();
-	printf("Control result: %d\r\n", controlResult);
-	printf("Elapsed time on CPU: %.3f ms\r\n", float(std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()) * 1e-6f);
-
-	thrust::device_vector<int> deviceNumbers = hostNumbers;
-	thrust::device_vector<int> intermediate(divCeil(divCeil(kNumbers, size_t(2)), kMaxBlockSize));
-	auto x = intermediate.size();
+	auto totalNumbers = data.size();
+	thrust::device_vector<float> deviceNumbers(data.data(), data.data() + data.size());
+	thrust::device_vector<float> intermediate(divCeil(divCeil(totalNumbers, size_t(2)), kMaxBlockSize));
+	//auto x = intermediate.size();
 
 	cudaError_t status;
 
@@ -92,21 +75,21 @@ int main(int argc, char** argv)
 	status = cudaEventCreate(&stop);
 	assert(status == cudaSuccess);
 
-	status = cudaEventRecord(start);
-	assert(status == cudaSuccess);
-
-
 	auto buffer1 = thrust::raw_pointer_cast(deviceNumbers.data());
 	auto buffer2 = thrust::raw_pointer_cast(intermediate.data());
 
-	for (size_t numbers = kNumbers; numbers > 1;)
+	printf("Parallel GPU started...\r\n");
+	status = cudaEventRecord(start);
+	assert(status == cudaSuccess);
+
+	for (size_t numbers = totalNumbers; numbers > 1;)
 	{
 		size_t pairs = divCeil(numbers, size_t(2));
 		size_t warps = divCeil(pairs, size_t(32));
-		dim3 blockSize(min(kMaxBlockSize, warps * size_t(32)));
-		dim3 gridSize(divCeil(pairs, size_t(blockSize.x)));
+		dim3 blockSize(unsigned(min(kMaxBlockSize, warps * size_t(32))));
+		dim3 gridSize(unsigned(divCeil(pairs, size_t(blockSize.x))));
 
-		reduceKernel <<<gridSize, blockSize, blockSize.x / 32 * sizeof(int)>>> (buffer1, numbers, buffer2);
+		reduceKernel <<<gridSize, blockSize, blockSize.x / 32 * sizeof(float)>>> (buffer1, numbers, buffer2);
 		//cudaDeviceSynchronize();
 		std::swap(buffer1, buffer2);
 		numbers = gridSize.x;
@@ -115,7 +98,7 @@ int main(int argc, char** argv)
 	status = cudaEventRecord(stop);
 	assert(status == cudaSuccess);
 
-	int result;
+	float result;
 	status = cudaMemcpy(&result, buffer1, sizeof(result), cudaMemcpyDeviceToHost);
 	assert(status == cudaSuccess);
 
@@ -127,17 +110,12 @@ int main(int argc, char** argv)
 	assert(status == cudaSuccess);
 
 	printf("Elapsed time on GPU: %.3f ms\r\n", ms);
-	printf("Result: %d\r\n", result);
-	deviceNumbers = hostNumbers;
-	printf("Result2: %d\r\n", thrust::reduce(deviceNumbers.begin(), deviceNumbers.end()));
+	printf("Result: %.3f\r\n", result);
+	//deviceNumbers = thrust::host_vector<float>(data);
+	//printf("Result2: %.3f\r\n", thrust::reduce(deviceNumbers.begin(), deviceNumbers.end()));
 
 	status = cudaEventDestroy(stop);
 	assert(status == cudaSuccess);
 	status = cudaEventDestroy(start);
 	assert(status == cudaSuccess);
-
-	assert(controlResult == result);
-
-	system("pause");
-	return 0;
 }
